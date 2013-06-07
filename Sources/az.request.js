412b5ac4eb16e528c766193b744639dded41ca0f
Yassin <yassin@atelier-zuppinger.ch>
2013-06-07 12:28:37 +0200

    reSend request with confirm param

--- Sources/az.request.js
+++ Sources/az.request.js
@@ -7,6 +7,8 @@ authors: AtelierZuppinger:@fingerflow
 provides: [AZ.Request]
 requires:
   - AZ/AZ
+  - Core/Class
+  - Core/Element.Event
   - Core/Request.JSON
   - Notification/Notification
 
@@ -17,8 +19,82 @@ AZ.Request = new Class({
 
 	Extends: Request.JSON,
 	
-	onSuccess: function(){
-		this.parent(arguments);
+	options: {/*
+		onError: function(){},
+	*/
+	},
+	
+	initialize: function(options){
+		this.notification = new Roar({
+			position: 'upperRight',
+			duration: 50000
+		});
+		this.parent(options);
+	},
+
+	onSuccess: function(response, xml){
+
+		if (response.success)
+			this.parent(response, xml);
+		else
+			this.onError(response, xml);
+
+		if (response.notifications)
+			this.triggerNotifications(response.notifications, response.params);
+
+	},
+
+	onError: function(response, xml){
+
+		this.fireEvent('error', arguments);
+
+	},
+
+	triggerNotifications: function(notifications, params){
+
+		var reSend = (function(confirmed){
+			this.reSend(confirmed, params);
+		}).bind(this);
+
+		notifications.each( function(notification){
+			
+			if (notification.options.type == 'alert')
+				this.notification.alert( notification.options.level, notification.options.message );
+			else 
+				this.notification.confirm(notification.options.level, notification.options.message, {
+					onConfirm: reSend
+				});
+
+		}, this);
+
+	}, 
+
+	reSend: function(confirmed, params){
+		params.confirmed = confirmed;
+		this.send(JSON.encode(params));
 	}
 	
+});
+
+window.addEvent('load', function(){
+	var result = document.id('result'),
+		sendrequest = document.id('sendrequest'),
+		success = function(response){
+			console.log('success √');
+		},
+		error = function(response){
+			console.log('error X');
+		},
+		request = new AZ.Request({
+			url: '/ajax/notification',
+			onSuccess: success,
+			onError: error
+		});
+
+	sendrequest.addEvent('click', function(){
+		request.send(JSON.encode({
+			param: 1
+		}));
+	})
+
 });
